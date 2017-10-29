4b43f688b0fafec25ef4ed5ed5fbffd749481888
Janarth <janarthp10@gmail.com>
2017-10-29 14:50:18 -0400

    init

---
+++ Receiver.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import sys
+import socket
+import struct
+import select
+import time
+
+DATA_PACKET_TYPE = 0
+ACK_PACKET_TYPE = 1
+EOT_PACKET_TYPE = 2
+
+DUMMY_IP = "0.0.0.0"
+DUMMY_PORT = 501
+
+RECEIVER_INFO_FILE = "recvInfo"
+CHANNEL_INFO_FILE = "channelInfo"
+
+
+def receive_go_back_n(filename):
+    expt_seq_num = 1
+    receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    receiver_socket.sendto("dummy", (DUMMY_IP, DUMMY_PORT))  # this lets the OS assign a port number to this socket
+
+    with open(RECEIVER_INFO_FILE, 'w') as f:
+        f.write("{0} {1}\n".format(receiver_socket.getsockname()[0], receiver_socket.getsockname()[1]))
+    print "RECEIVER: ", receiver_socket.getsockname()
+
+    file_to_write = open(filename, 'wb')
+    while True:
+        print "blocking while waiting for incoming packet"
+        readers, _, _ = select.select([receiver_socket], [], [])
+        data, addr = readers[0].recvfrom(512)
+        header = struct.unpack('>III', data[:12])
+        print "header: ", header
+        print "expected: ", expt_seq_num
+        if header[0] == DATA_PACKET_TYPE:
+            payload = struct.unpack('>{0}s'.format(header[1] - 12), data[12:header[1]])
+            file_to_write.write(payload[0])
+            print "recvd:\n", payload
+            if header[2] == expt_seq_num:
+                print "got expected seq"
+                ack_packet = struct.pack('>III', ACK_PACKET_TYPE, 12, expt_seq_num)
+                receiver_socket.sendto(ack_packet, (addr[0], addr[1]))
+            elif expt_seq_num > 1:
+                print "wrong order, header2 = ", header[2]
+                ack_packet = struct.pack('>III', ACK_PACKET_TYPE, 12, expt_seq_num-1)
+                receiver_socket.sendto(ack_packet, (addr[0], addr[1]))
+        elif header[0] == EOT_PACKET_TYPE:
+            eot_packet = struct.pack('>III', EOT_PACKET_TYPE, 12, 0)
+            receiver_socket.sendto(eot_packet, (addr[0], addr[1]))
+            file_to_write.close()
+            sys.exit()
+
+
+if len(sys.argv) != 3:
+    sys.exit("Error: Expected 2 arguments")
+
+protocol_selector = int(sys.argv[1])
+filename = sys.argv[2]
+
+receive_go_back_n(filename)
---
+++ test_function.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+import socket
+from struct import *
+import time
+import sys
+import signal
+import select
+
+DATA_PACKET_TYPE = 0
+ACK_PACKET_TYPE = 1
+EOT_PACKET_TYPE = 2
+
+DUMMY_IP = "0.0.0.0"
+DUMMY_PORT = 500
+
+CHANNEL_INFO_FILE = "channelInfo"
+MAX_PAYLOAD = 10
+
+
+# timeout in milliseconds
+def go_back_n(filename, utimeout, window_size):
+    base = next_seq_num = 1
+    timeout = utimeout/1000
+    window = []
+
+    sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    for i in range(6):  # try opening channelInfo for 1 minute
+        try:
+            with open(CHANNEL_INFO_FILE, 'r') as f:
+                temp = f.readline().split(' ')
+                channel_info = (temp[0], int(temp[1]))
+                break
+        except IOError as e:
+            time.sleep(10)  # wait for user to run channel script
+    if 'channel_info' in locals():
+        print "channel: ", channel_info
+    else:
+        sys.exit("Error: Could not retrieve channelInfo")
+
+    def timeout_handler(signum, frame):
+        print "timed out in handler"
+        signal.setitimer(signal.ITIMER_REAL, timeout)
+        for i in range(base, next_seq_num):
+            print "i: ", i
+            sender_socket.sendto(window[i - 1], (channel_info[0], channel_info[1]))
+
+    signal.signal(signal.SIGALRM, timeout_handler)
+    file_to_send = open(filename, 'rb')
+    while True:
+        try:
+            print "in try"
+            readers, _, _ = select.select([sender_socket], [], [], timeout/10)
+            if len(readers) == 1:
+                print "in select if statement"
+                data, addr = readers[0].recvfrom(12)  # since sender only recieves ack and eots
+                header = unpack('>III', data[:12])
+                print header
+                if header[0] == ACK_PACKET_TYPE & header[2] + 1 > base:  # ignore dup acks
+                    base = header[2] + 1
+                    if base == next_seq_num and file_to_send.closed:
+                        signal.setitimer(signal.ITIMER_REAL, 0)
+                        eot_packet = pack('>III', EOT_PACKET_TYPE, 12, 0)
+                        sender_socket.sendto(eot_packet, (channel_info[0], channel_info[1]))
+                    else:
+                        signal.setitimer(signal.ITIMER_REAL, timeout)
+                else:  # EOT response
+                    sys.exit()
+        except select.error:
+            pass
+
+        print "after try"
+        if (next_seq_num < base + window_size) and not file_to_send.closed:
+            payload = file_to_send.read(MAX_PAYLOAD)
+            print "payload:\n", payload
+
+            if payload == "":
+                file_to_send.close()
+            else:
+                fmt = '>III{0}s'.format(len(payload))
+
+                packet = pack(fmt, DATA_PACKET_TYPE, calcsize(fmt), next_seq_num, payload)
+                window.append(packet)
+                sender_socket.sendto(packet, (channel_info[0], channel_info[1]))
+                if base == next_seq_num:
+                    signal.setitimer(signal.ITIMER_REAL, timeout)
+                    print "set timeout to: ", signal.getitimer(signal.ITIMER_REAL)
+                next_seq_num += 1
+
+
+def selective_repeat(filename, timeout, window_size):
+    base, next_seq_num = 1
+    with open(filename, 'rb') as f:
+        while True:
+            if next_seq_num < base + window_size:
+                payload = f.read(500)
+
+                if payload == "":
+                    break
+
+                fmt = '>iii{0}s'.format(len(payload))
+
+                packet = pack(fmt, DATA_PACKET_TYPE, calcsize(fmt), next_seq_num, payload)
+                unpacked = unpack(fmt, packet)
