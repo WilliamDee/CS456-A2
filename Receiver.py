32a4b8fe782e6daf148e109f4be5f8a7cb56305e
Janarth <janarthp10@gmail.com>
2017-10-31 03:01:24 -0400

    implemented recv for selective repeat

--- Receiver.py
+++ Receiver.py
@@ -8,6 +8,7 @@ import select
 DATA_PACKET_TYPE = 0
 ACK_PACKET_TYPE = 1
 EOT_PACKET_TYPE = 2
+WINDOW_SIZE = 10
 
 DUMMY_IP = "0.0.0.0"
 DUMMY_PORT = 0
@@ -79,10 +80,57 @@ def receive_go_back_n(filename):
             sys.exit()
 
 
+def receive_selective_repeat(filename):
+    rcv_base = 1
+    window = {}
+    receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    receiver_socket.bind((DUMMY_IP, DUMMY_PORT))
+
+    with open(RECEIVER_INFO_FILE, 'w') as f:
+        f.write("{0} {1}\n".format(receiver_socket.getsockname()[0], receiver_socket.getsockname()[1]))
+    # print "RECEIVER: ", receiver_socket.getsockname()
+
+    file_to_write = open(filename, 'ab')
+    while True:
+        print "blocking while waiting for incoming packet"
+        readers, _, _ = select.select([receiver_socket], [], [])
+        data, addr = readers[0].recvfrom(512)
+        header = struct.unpack('>III', data[:12])
+        log(header, False)
+        if header[0] == DATA_PACKET_TYPE and header[2] < rcv_base + WINDOW_SIZE:
+            payload = struct.unpack('>{0}s'.format(header[1] - 12), data[12:header[1]])
+            if header[2] == rcv_base:
+                file_to_write.write(payload[0])
+                last_inorder_seq = header[2]
+                for key in window.keys():
+                    if key == last_inorder_seq + 1:
+                        file_to_write.write(window[key])
+                        window.pop(key)
+                        last_inorder_seq = key
+                    else:
+                        break
+
+            elif len(window) < WINDOW_SIZE and header[2] not in window.keys():
+                window[header[2]] = payload
+
+            ack_packet = struct.pack('>III', ACK_PACKET_TYPE, 12, header[2])
+            receiver_socket.sendto(ack_packet, (addr[0], addr[1]))
+            log((ACK_PACKET_TYPE, 12, header[2]), True)
+        elif header[0] == EOT_PACKET_TYPE:
+            eot_packet = struct.pack('>III', EOT_PACKET_TYPE, 12, 0)
+            receiver_socket.sendto(eot_packet, (addr[0], addr[1]))
+            log((EOT_PACKET_TYPE, 12, 0), True)
+            file_to_write.close()
+            sys.exit()
+
+
 if len(sys.argv) != 3:
     sys.exit("Error: Expected 2 arguments")
 
 protocol_selector = int(sys.argv[1])
 filename = sys.argv[2]
 
-receive_go_back_n(filename)
+if protocol_selector == 0:
+    receive_go_back_n(filename)
+else:
+    receive_selective_repeat(filename)
