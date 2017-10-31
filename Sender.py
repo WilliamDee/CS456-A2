a7f030cb1c969f7a84d21371c0b6765a29bab517
Janarth <janarthp10@gmail.com>
2017-10-31 01:02:52 -0400

    testing selective repeat progress

--- Sender.py
+++ Sender.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
 
-from test_function import go_back_n
+from test_function import go_back_n, selective_repeat
 import sys
 
 if len(sys.argv) != 4:
@@ -10,8 +10,7 @@ protocol_selector = int(sys.argv[1])
 timeout = int(sys.argv[2])
 filename = sys.argv[3]
 
-WINDOW_SIZE = 10
-
-go_back_n(filename, timeout, WINDOW_SIZE)
+#go_back_n(filename, timeout)
+selective_repeat(filename, timeout)
 
 
